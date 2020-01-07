fc0af6d762750a04cdfe6b66a399b97248afc58b
Giovel Soriano Ranoa <giovelsorianoranoa@cn-mbp-1132.local>
2020-01-07 18:22:58 +0800

    1

---
+++ Java Training/rootController/src/test/java/com/javatraining/rootController/RootControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatraining.rootController;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RootControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
